af15cbd61dcafcd0619ccee6a26ce078a1920104
Maciej Zuralski <maciejzuralski13@icloud.com>
2024-04-14 16:30:23 +0200

    Create matrix A and B

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Maciej Żuralski 193367
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
+# Special function for exercise A
+def special_function(x):
+    # f + 1 = 3 + 1 = 4
+    return math.sin(x * 4)
+
+
+# Exercise A - create matrix equation
+def create_matrix_equation(matrix_size, fun):
+    a1 = 8  # 5 + 3 = 8
+    a2 = -1
+    a3 = -1
+
+    a_matrix_new = np.zeros((matrix_size, matrix_size))
+    b_matrix_new = np.zeros((matrix_size, 1))
+
+    for i in range(matrix_size - 2):
+        a_matrix_new[i][i] = a1
+        a_matrix_new[i + 1][i] = a2
+        a_matrix_new[i][i + 1] = a2
+        a_matrix_new[i + 2][i] = a3
+        a_matrix_new[i][i + 2] = a3
+
+        b_matrix_new[i][0] = fun(i)
+
+    a_matrix_new[matrix_size - 2][matrix_size - 2] = a1
+    a_matrix_new[matrix_size - 1][matrix_size - 1] = a1
+    a_matrix_new[matrix_size - 2][matrix_size - 1] = a2
+    a_matrix_new[matrix_size - 1][matrix_size - 2] = a2
+
+    b_matrix_new[matrix_size - 2][0] = fun(matrix_size - 2)
+    b_matrix_new[matrix_size - 1][0] = fun(matrix_size - 1)
+
+    return a_matrix_new, b_matrix_new
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    n = 967
+    a_matrix, b_matrix = create_matrix_equation(n, special_function)
