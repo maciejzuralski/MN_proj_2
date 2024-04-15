a38d758cc85c4d413de15437b639acbe05ec67f4
Maciej Zuralski <maciejzuralski13@icloud.com>
2024-04-15 14:44:12 +0200

    Multiplicate matrix A and B

--- main.py
+++ main.py
@@ -10,14 +10,30 @@ def special_function(x):
     return math.sin(x * 4)
 
 
+# Matrix Multiplication
+def matrix_multiplication(matrix_1, matrix_2):
+    mat1_high = len(matrix_1)
+    mat2_high = len(matrix_2)
+    mat1_width = len(matrix_1[0])
+    mat2_width = len(matrix_2[0])
+
+    new_matrix = [[0 for _ in range(mat1_high)] for _ in range(mat2_width)]
+
+    for y in range(mat1_high):
+        for x in range(mat2_width):
+            new_matrix[y][x] = sum(matrix_1[y][i] * matrix_2[i][x] for i in range(mat1_width))
+
+    return  new_matrix
+
+
 # Exercise A - create matrix equation
 def create_matrix_equation(matrix_size, fun):
     a1 = 8  # 5 + 3 = 8
     a2 = -1
     a3 = -1
 
-    a_matrix_new = np.zeros((matrix_size, matrix_size))
-    b_matrix_new = np.zeros((matrix_size, 1))
+    a_matrix_new = [[0 for _ in range(n)] for _ in range(n)]
+    b_matrix_new = [[0] for _ in range(n)]
 
     for i in range(matrix_size - 2):
         a_matrix_new[i][i] = a1
@@ -42,3 +58,6 @@ def create_matrix_equation(matrix_size, fun):
 if __name__ == '__main__':
     n = 967
     a_matrix, b_matrix = create_matrix_equation(n, special_function)
+    a = [[1], [2]]
+    b = [[3, 4]]
+    c = matrix_multiplication(a, b)
